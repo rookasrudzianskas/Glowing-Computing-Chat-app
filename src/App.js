eac4acf7888f674d0b0dea8a7df71375a0c723a6
Rokas <rookas.rudzenskas@gmail.com>
2021-04-13 14:27:41 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import 'rsuite/lib/styles/index.less';
+// import 'rsuite/lib/styles/index.less';
+import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
-// import SignIn from './pages/SignIn';
-// import { Switch } from "react-router";
-// import PrivateRoute from './components/PrivateRoute';
-// import Home from './pages/Home';
-// import PublicRoute from './components/PublicRoute';
+import SignIn from './pages/SignIn';
+import { Switch } from "react-router";
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return (
-      <div>
-        Rokas
-      </div>
-      // <Switch>
-      //   <PublicRoute path={"/signin"}>
-      //     <SignIn />
-      //   </PublicRoute>
-      //   <PrivateRoute path="/">
-      //     <Home />
-      //   </PrivateRoute>
-      // </Switch>
-  );
+    return (
+        <Switch>
+            <PublicRoute path="/signin">
+                <SignIn />
+            </PublicRoute>
+            <PrivateRoute path="/">
+                <Home />
+            </PrivateRoute>
+        </Switch>
+    );
 }
 
 export default App;
---
+++ src/pages/SignIn.js
@@ -0,0 +1,50 @@
+import React from 'react';
+// import 'rsuite/dist/styles/rsuite-default.css';
+// import firebase from 'firebase/app';
+// import { Container, Grid, Row, Panel, Col, Button, Icon } from 'rsuite';
+// import { auth } from '../misc/firebase';
+
+const SignIn = () => {
+
+    // const signInWithProvider = async (provider) => {
+    //     const result = await auth.signInWithPopup(provider);
+    //     console.log(result)
+    // }
+    //
+    //
+    // const onFacebookSignIn = () => {
+    //     signInWithProvider( new firebase.auth.FacebookAuthProvider() );
+    // };
+    // const onGoogleSignIn = () => {
+    //     signInWithProvider( new firebase.auth.GoogleAuthProvider() );
+    // };
+
+    return (
+        // <Container>
+        //     <Grid className="mt-page">
+        //         <Row>
+        //             <Col xs={24} md={12} mdOffset={6}>
+        //                 <Panel>
+        //                     <div className="text-center">
+        //                         <h2>Welcome to Chat</h2>
+        //                         <p>Progressive chat platform for Developers</p>
+        //                     </div>
+        //
+        //                     <div className="mt-3">
+        //                         <Button block color="blue" onClick={onFacebookSignIn}>
+        //                             <Icon icon="facebook" /> Continue with Facebook
+        //                         </Button>
+        //                         <Button block color="green" onClick={onGoogleSignIn}>
+        //                             <Icon icon="google" /> Continue with Google
+        //                         </Button>
+        //                     </div>
+        //                 </Panel>
+        //             </Col>
+        //         </Row>
+        //     </Grid>
+        // </Container>
+        <div>sign in</div>
+    );
+};
+
+export default SignIn;
